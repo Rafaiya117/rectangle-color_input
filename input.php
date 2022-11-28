1eef524844d4f5e052bb04262a634cca14166cc1
rafaiya <rushdha@alistlimited.com>
2022-11-28 12:57:06 +0600

    commited

---
+++ input.php
@@ -0,0 +1,23 @@
+<!-- HTML -->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Rectangle</title>
+</head>
+<!-- CSS -->
+<link rel="stylesheet" href="rectangle.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<body style=" background-image: url(color-code.jpg);background-size:cover;">
+<h1 class="h1">Color by User input</h1>
+
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+        <p> <a class="advanced-input"><label for="color-2">Color:</label><input id="color-2" class="form-control" type="text" value="" /></a><br></p>
+         <p> <a class="advanced"  href="#" style="background-color:;"></a></p>
+        </div>
+</div>
+</div>
+</body>
+</html>
+<script src="rectangle.js"></script>
